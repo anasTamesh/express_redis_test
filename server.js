1edac432cc0ce8b46370467b1adce48ef39de8b0
anasTamesh <37302097+anasTamesh@users.noreply.github.com>
2022-05-12 01:36:45 +0300

    Removed unwanted commints

--- server.js
+++ server.js
@@ -21,16 +21,6 @@ app.get("/photos", async (req, res) => {
         return data;
     });
     res.json(photos);
-    // redisClient.get('photos', async (error, photos) => {
-    //     if(error) console.error(error)
-    //     if(photos != null){
-    //         return res.json(JSON.parse(photos));
-    //     } else {
-    //         redisClient.setex('photos', DEFAULT_EXPIRATION, JSON.stringify(data))
-    //         res.json(data);
-    //     }
-    // });
-
 });
 
 app.get("/photos/:id", async (req, res) => {
